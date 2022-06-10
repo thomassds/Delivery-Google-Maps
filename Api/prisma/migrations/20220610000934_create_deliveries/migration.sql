28f14134d87be4191e8291f231e1ba3547b51cd2
Thomas Domingos <thomasdomingos@hotmail.com>
2022-06-09 21:12:36 -0300

    deploy

--- Api/prisma/migrations/20220610000934_create_deliveries/migration.sql
+++ Api/prisma/migrations/20220610000934_create_deliveries/migration.sql
@@ -1,26 +1,4 @@
 -- CreateTable
-CREATE TABLE "clients" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
-    "email" TEXT NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "adresses" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "cep" TEXT NOT NULL,
-    "street" TEXT NOT NULL,
-    "number" TEXT NOT NULL,
-    "district" TEXT NOT NULL,
-    "complement" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "longitude" TEXT NOT NULL,
-    "latitude" TEXT NOT NULL
-);
-
--- CreateTable
 CREATE TABLE "deliveries" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "delivery_date" DATETIME NOT NULL,
